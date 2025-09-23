156c456ed2f0894ede3bb9967156af395ec24355
selmas1 <selmas@stumail.jccc.edu>
2025-09-22 22:53:00 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,28 @@
+/*Selin Elmas, Chapter 3 assignment, September 22, 2025*/
+
+
+const fullName = "Selin Elmas";
+console.log("Full Name:", fullName);
+
+
+const desiredAnnualSalary = 275000;
+console.log("Desired Annual Salary (USD):", desiredAnnualSalary);
+
+const isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+
+const friends = ["Ilayda", "Merve", "Irem"];
+console.log("Friends:", friends);
+
+
+const friendDesiredSalaries = [275000, 300000, 400000];
+console.log("Friends' Desired Salaries (USD):", friendDesiredSalaries);
+
+// 6) Literal object: another friend
+const anotherFriend = {
+  firstName: "Mo",
+  lastName: "Ramsey",
+  desiredAnnualSalary: 300000
+};
+console.log("Another Friend:", anotherFriend);
